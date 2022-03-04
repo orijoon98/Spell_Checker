b3c8f329d21715b396af867087ad0f119e80344a
HyeokJoon Kong <orijoon98@gmail.com>
2022-03-04 10:15:38 +0900

    Feat: hanspell 추가

---
+++ backend/checker.js
@@ -0,0 +1,19 @@
+const hanspell = require('hanspell');
+const logger = require('./logger');
+
+const checker = (sentence) => {
+  return new Promise((resolve, reject) => {
+    const end = function () {};
+    const error = function (err) {
+      logger.error(err);
+      reject(err);
+    };
+    const response = function (res) {
+      logger.info('spell checked');
+      resolve(res);
+    };
+    hanspell.spellCheckByPNU(sentence, 10000, response, end, error);
+  });
+};
+
+module.exports = checker;
